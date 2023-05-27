a89387d40f4296ad9853b5908b3e720f16fbdea5
VanessaCabelli <vanessacabelli@gmail.com>
2023-05-27 14:23:08 -0300

    Alteração home e historico de peso

--- src/web/src/app/dashboard/historico-de-peso/page.jsx
+++ src/web/src/app/dashboard/historico-de-peso/page.jsx
@@ -1,9 +1,75 @@
+"use client";
+import { useState } from "react";
+
 function historicoDePeso() {
-  return (
-    <div>
-      Histórico de Peso
-    </div>
-  )
+    const [peso, setPeso] = useState(0);
+
+    const [historico, setHistorico] = useState([]);
+
+    function adicionarPeso() {
+        setHistorico([
+            ...historico,
+            {
+                peso,
+                data: new Date(),
+            },
+        ]);
+        setPeso(0);
+    }
+
+    return (
+        <div className="w-screen min-h-screen bg-teal-500 h-full flex flex-col items-center p-12 gap-10 justify-center">
+            <div className="flex flex-col wrap w-2/3>">
+                <h1 className="mx-auto mb-4 bg-theme-blue p-4 rounded-lg font-bold text-2xl">
+                    Controle de Peso
+                </h1>
+                <div>
+                    <input
+                        value={peso}
+                        onChange={(e) => setPeso(e.target.value)}
+                        type="number"
+                        placeholder="Digite seu peso atual"
+                        className="bg-white px-2 py-1 text-black rounded-md"
+                    />
+                    <button
+                        onClick={adicionarPeso}
+                        className="bg-teal-600 px-2 py-1 text-white rounded-md"
+                    >
+                        Adicionar
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                <table className="table table-auto bg-white ">
+                    <thead>
+                        <th className="border border-spacing-y-2 text-center min-w-[150px] p-2">
+                            PESO
+                        </th>
+                        <th className="border border-spacing-y-2 text-center min-w-[150px] p-2">
+                            DATA
+                        </th>
+                    </thead>
+                    <tbody>
+                        {true &&
+                            historico.map((registro) => (
+                                <tr key={registro.data}>
+                                    <td className="border border-spacing-y-2 text-center min-w-[150px] p-2">
+                                        {registro.peso}
+                                    </td>
+                                    <td className="border border-spacing-y-2 text-center min-w-[150px] p-2">
+                                        {registro.data.toLocaleDateString(
+                                            "pt-BR",
+                                            { week: "long" }
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
-export default historicoDePeso
+export default historicoDePeso;
--- src/web/src/app/page.jsx
+++ src/web/src/app/page.jsx
@@ -1,87 +1,142 @@
-'use client'
+"use client";
 import { z } from "zod";
 
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
+import { Inter } from "next/font/google";
+import Link from "next/link";
 import { Toaster, toast } from "react-hot-toast";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const formSchema = z.object({
-  email: z.string().email('Endereço de e-mail inválido').min(1, "Email obrigatório"),
-  password: z.string().min(1, "Senha obrigatória").min(8, "Senha deve possuir pelo menos 8 caractéres"),
-})
+    email: z
+        .string()
+        .email("Endereço de e-mail inválido")
+        .min(1, "Email obrigatório"),
+    password: z
+        .string()
+        .min(1, "Senha obrigatória")
+        .min(8, "Senha deve possuir pelo menos 8 caractéres"),
+});
 
 export default function Home() {
-  const router = useRouter()
-  
-  async function handleSubmit(event) {
-    event.preventDefault()
+    const router = useRouter();
 
-    const formData = new FormData(event.target)
-    const credentials = Object.fromEntries(formData)
+    async function handleSubmit(event) {
+        event.preventDefault();
 
-    const user = localStorage.getItem('nutripuc[user]')
+        const formData = new FormData(event.target);
+        const credentials = Object.fromEntries(formData);
 
-    if (user != credentials.email) {
-      toast.error('Usuário não cadastrado')
+        const user = localStorage.getItem("nutripuc[user]");
 
-      return
+        if (user != credentials.email) {
+            toast.error("Usuário não cadastrado");
+
+            return;
+        }
+
+        const storedPassword = localStorage.getItem("nutripuc[password]");
+
+        const response = await fetch("/api/login", {
+            cache: "no-store",
+            method: "POST",
+            body: JSON.stringify({
+                password: credentials.password,
+                storedPassword,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
+
+        const { authSuccess } = await response.json();
+        console.log(authSuccess);
+
+        if (authSuccess == true) {
+            router.push("/dashboard");
+        } else {
+            toast.error("Senha inválida!");
+        }
     }
 
-  const storedPassword = localStorage.getItem('nutripuc[password]')
+    return (
+        <main className="grid grid-cols-1 lg:grid-cols-2 my-36 items-center bg-teal-100">
+            <div className="min-w-[320px] max-w-[60ch] justify-self-center justify-center items-center flex flex-col md:items-start md:justify-start">
+                <h2 className="md:text-9xl text-center text-7xl text-gray-50 italic font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.25)]">
+                    Nutripuc
+                </h2>
+                <h3 className="text-2xl md:text-5xl text-center md:text-start  font-semibold text-slate-500 my-4 max-w-[15ch]">
+                    Seu aplicativo de saúde geral
+                </h3>
+                <p className="text-center items-center self-center md:text-left md:self-start my-4 max-w-[35ch]">
+                    Estimular a saúde, a qualidade de vida e a prevenção de
+                    doenças visando à longevidade. Promover uma relação saudável
+                    entre o paciente e o alimento, respeitando preferências e
+                    individualidades. Sem fazer terrorismo nutricional faz parte
+                    do nosso objetivo!
+                </p>
+            </div>
+            <div className="min-w-[320px] justify-self-center">
+                <form
+                    method="POST"
+                    onSubmit={handleSubmit}
+                    className="bg-teal-500/25 shadow-2xl p-10 border-2 border-teal-500/10 rounded-xl"
+                >
+                    <h3 className="font-bold text-teal-600 text-5xl text-center p-2 m-4 mb-8">
+                        Faça seu login
+                    </h3>
 
-  const response = await fetch('/api/login', {
-    cache: 'no-store',
-    method: "POST",
-    body: JSON.stringify({
-      password: credentials.password,
-      storedPassword,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Type": "application/x-www-form-urlencoded",
-    }
-  })
+                    <label
+                        htmlFor="email"
+                        className="block text-teal-600 font-bold my-2 text-lg"
+                    >
+                        Email
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Email cadastrado"
+                        id="email"
+                        name="email"
+                        className="w-full px-4 py-2 rounded-md"
+                    />
 
-    const { authSuccess } = await response.json()
-    console.log(authSuccess)
+                    <label
+                        htmlFor="password"
+                        className="block text-teal-600 font-bold my-2 mt-4 text-lg"
+                    >
+                        Senha
+                    </label>
+                    <input
+                        type="password"
+                        autoComplete="new-password"
+                        placeholder="Senha"
+                        name="password"
+                        id="password"
+                        className="w-full px-4 py-2 rounded-md"
+                    />
 
-    if (authSuccess == true) {
-      router.push('/dashboard')
-    } else {
-      toast.error('Senha inválida!')
-    }
-
-  }
-
-  return (
-    <main className='grid grid-cols-1 lg:grid-cols-2 my-36 items-center bg-teal-100'>
-      <div className='min-w-[320px] max-w-[60ch] justify-self-center justify-center items-center flex flex-col md:items-start md:justify-start'>
-        <h2 className='md:text-9xl text-center text-7xl text-gray-50 italic font-bold drop-shadow-[2px_2px_2px_rgba(0,0,0,0.25)]'>Nutripuc</h2>
-        <h3 className='text-2xl md:text-5xl text-center md:text-start  font-semibold text-slate-500 my-4 max-w-[15ch]'>Seu aplicativo de saúde geral</h3>
-        <p className='text-center items-center self-center md:text-left md:self-start my-4 max-w-[35ch]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ullam quam voluptatibus deleniti reprehenderit enim esse sit et voluptates ad! </p>
-      </div>
-      <div className='min-w-[320px] justify-self-center'>
-        <form method="POST" onSubmit={handleSubmit} className='bg-teal-500/25 shadow-2xl p-10 border-2 border-teal-500/10 rounded-xl'>
-          <h3 className='font-bold text-teal-600 text-5xl text-center p-2 m-4 mb-8'>Faça seu login</h3>
-
-          <label htmlFor="email" className='block text-teal-600 font-bold my-2 text-lg'>Email</label>
-          <input type="text" placeholder="Email cadastrado" id="email" name="email" className='w-full px-4 py-2 rounded-md' />
-
-          <label htmlFor="password" className='block text-teal-600 font-bold my-2 mt-4 text-lg'>Senha</label>
-          <input type="password" autoComplete='new-password' placeholder="Senha" name="password" id="password" className='w-full px-4 py-2 rounded-md' /> 
-
-          <button type='submit' className='block w-full bg-teal-500 hover:bg-teal-600 rounded-md mt-8 text-white font-bold tracking-wider p-4 text-2xl'>Entrar</button>
-          <fieldset className="border-t border-slate-400 text-center py-4 mt-8">
-            <legend className="mx-auto px-4 text-slate-400 text-2xl italic">Não tem cadastro?</legend>
-            <Link href="/sign-up" className="text-teal-500 text-lg hover:text-white">Faça cadastro agora</Link>
-          </fieldset>
-          <Toaster />
-        </form>
-      </div>
-    </main>
-  )
+                    <button
+                        type="submit"
+                        className="block w-full bg-teal-500 hover:bg-teal-600 rounded-md mt-8 text-white font-bold tracking-wider p-4 text-2xl"
+                    >
+                        Entrar
+                    </button>
+                    <fieldset className="border-t border-slate-400 text-center py-4 mt-8">
+                        <legend className="mx-auto px-4 text-slate-400 text-2xl italic">
+                            Não tem cadastro?
+                        </legend>
+                        <Link
+                            href="/sign-up"
+                            className="text-teal-500 text-lg hover:text-white"
+                        >
+                            Faça cadastro agora
+                        </Link>
+                    </fieldset>
+                    <Toaster />
+                </form>
+            </div>
+        </main>
+    );
 }
